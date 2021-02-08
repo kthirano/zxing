bde8514a71f609e721db247614733c2173b09672
kthirano <kthirano@uci.edu>
2021-02-07 20:51:42 -0800

    part 2: functional tests

---
+++ core/src/test/java/com/google/zxing/aztec/AztecFailTheseTest.java
@@ -0,0 +1,14 @@
+package com.google.zxing.aztec;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.common.AbstractBlackBoxTestCase;
+
+public class AztecFailTheseTest extends AbstractBlackBoxTestCase{
+  public AztecFailTheseTest(){
+    super("src/test/resources/blackbox/aztec-fail", new AztecReader(), BarcodeFormat.AZTEC);
+    addTest(0,0,0.0f);
+    addTest(0,0,90.0f);
+    addTest(0,0,180.0f);
+    addTest(0,0,270.0f);
+  }
+}
---
+++ core/src/test/java/com/google/zxing/aztec/AztecPassTheseTest.java
@@ -0,0 +1,14 @@
+package com.google.zxing.aztec;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.common.AbstractBlackBoxTestCase;
+
+public class AztecPassTheseTest extends AbstractBlackBoxTestCase{
+  public AztecPassTheseTest(){
+    super("src/test/resources/blackbox/aztec-pass", new AztecReader(), BarcodeFormat.AZTEC);
+    addTest(5,5,0.0f);
+    addTest(5,5,90.0f);
+    addTest(5,5,180.0f);
+    addTest(5,5,270.0f);
+  }
+}
