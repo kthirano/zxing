9275551424c7f1d9ff507f97dc88903b65930068
kthirano <kthirano@uci.edu>
2021-02-28 17:01:22 -0800

    testableDesign

--- core/src/test/java/com/google/zxing/aztec/decoder/AztecTestableDesign.java
+++ core/src/test/java/com/google/zxing/aztec/decoder/AztecTestableDesign.java
@@ -1,12 +1,7 @@
 package com.google.zxing.aztec.decoder;
 
-
-import com.google.zxing.aztec.encoder.EncoderTest;
-
-import com.google.zxing.FormatException;
 import com.google.zxing.ResultPoint;
 import com.google.zxing.aztec.AztecDetectorResult;
-import com.google.zxing.common.BitArray;
 import com.google.zxing.common.BitMatrix;
 
 import org.junit.Test;
@@ -17,7 +12,7 @@ public class AztecTestableDesign extends Assert {
   private static final ResultPoint[] NO_POINTS = new ResultPoint[0];
 
   @Test
-  public void testCorrectBits() throws Exception{
+  public void testCorrectBits(){
     BitMatrix matrix = BitMatrix.parse(
       "X X X X X     X X X       X X X     X X X     \n" +
         "X X X     X X X     X X X X     X X X     X X \n" +
@@ -66,6 +61,54 @@ public class AztecTestableDesign extends Assert {
       false, true, false, false, true, false, false, false, false, true, true, false, true, true, true, false,
       true, false, true, false, false, false, true, true, false, true, false, false, false, false, true, true};
     assertArrayEquals(b, actual);
+  }
+
+  @Test
+  public void testCorrectBits2(){
+    BitMatrix matrix = BitMatrix.parse(
+      "      X     X X X   X           X     \n" +
+        "    X X   X X   X X X X X X X   X     \n" +
+        "    X X                         X   X \n" +
+        "  X X X X X X X X X X X X X X X X X   \n" +
+        "      X                       X       \n" +
+        "      X   X X X X X X X X X   X   X   \n" +
+        "  X X X   X               X   X X X   \n" +
+        "  X   X   X   X X X X X   X   X X X   \n" +
+        "      X   X   X       X   X   X X X   \n" +
+        "  X   X   X   X   X   X   X   X   X   \n" +
+        "X   X X   X   X       X   X   X     X \n" +
+        "  X X X   X   X X X X X   X   X X     \n" +
+        "      X   X               X   X X   X \n" +
+        "      X   X X X X X X X X X   X   X X \n" +
+        "  X   X                       X       \n" +
+        "X X   X X X X X X X X X X X X X X X   \n" +
+        "X X     X   X         X X X       X X \n" +
+        "  X   X   X   X X X X X     X X   X   \n" +
+        "X     X       X X   X X X       X     \n",
+      "X ", "  ");
+    AztecDetectorResult r = new AztecDetectorResult(matrix, NO_POINTS, false, 30, 2);
+    Decoder d = new Decoder();
+    boolean[] b = d.testExtractBits(r, r.getBits());
+    boolean[] actual = {
+      false, false, false, false, false, false, false, true, false, false, false, false, false, true, false, true,
+      false, false, false, true, true, false, false, false, false, false, false, true, true, true, true, true, false,
+      true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+      false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+      false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+      false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+      false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+      false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+      false, false, false, true, true, false, false, false, true, false, true, false, true, true, true, false, true,
+      true, true, true, false, true, true, false, true, false, false, true, true, false, true, true, true, true,
+      true, false, true, false, true, true, true, false, true, false, true, false, true, true, true, false, true,
+      false, true, false, true, false, true, false, false, false, true, false, true, false, false, false, false,
+      false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+      false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+      false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+      false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+      true, false, false, true, true, true, false, true, false, true, false, true, false, true, false, true, false,
+      true, false, true, false, true, false, true, false, true, false, true,};
+    assertArrayEquals(b,actual);
   }
 
 
